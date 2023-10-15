2b4679d7492924f513af2d16e3e84123ce6e3745
byeongsoon <byeongsoon94@gmail.com>
2023-10-15 15:01:39 +0900

    react 과제 3번 프로젝트 연결

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+
+function App() {
+  return (
+      <section>
+        <div className="first-div">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="second-div">
+          <div></div>
+          <div></div>
+        </div>
+        <div className="third-div">
+          <div></div>
+        </div>
+        <div className="fourth-div"></div>
+        <div class="fifth-div">
+          <div></div>
+          <div></div>
+        </div>
+      </section>
+  );
+}
+
+export default App;
